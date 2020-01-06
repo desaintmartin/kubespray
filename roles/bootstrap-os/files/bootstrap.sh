22f354c91e4370982d8fa38ee4d822ffe0e46af3
Cédric de Saint Martin <cdesaintmartin@wiremind.fr>
2020-01-06 13:33:24 +0100

    Force Enable mitogen.

--- roles/bootstrap-os/files/bootstrap.sh
+++ roles/bootstrap-os/files/bootstrap.sh
@@ -7,14 +7,16 @@ mkdir -p $BINDIR
 
 cd $BINDIR
 
-if [[ -e $BINDIR/.bootstrapped ]]; then
+if [[ -e $BINDIR/.bootstrapped-2 ]]; then
   exit 0
 fi
 
-PYPY_VERSION=7.0.0
+rm -rf python pypy3
 
-wget -O - https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3.5-$PYPY_VERSION-linux_x86_64-portable.tar.bz2 | tar -xjf -
-mv -n pypy3.5-$PYPY_VERSION-linux_x86_64-portable pypy3
+PYPY_VERSION=7.2.0
+
+wget -O - https://github.com/squeaky-pl/portable-pypy/releases/download/pypy3.6-7.2.0/pypy3.6-$PYPY_VERSION-linux_x86_64-portable.tar.bz2 | tar -xjf -
+mv -n pypy3.6-$PYPY_VERSION-linux_x86_64-portable pypy3
 
 ln -s ./pypy3/bin/pypy3 python
 $BINDIR/python --version
